818c14918d51a4155520449bd45e0ad19007fe63
saurabhcool001 <parmarsaurabh87@gmail.com>
2019-02-22 13:57:31 +0530

    This is basic where click on button take video from android gallery and show on activity with video path on actvity.

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.videoplayer;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button LoadVid;
     VideoView videoView;
+    TextView videoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         LoadVid = findViewById(R.id.LoadVid);
         videoView = findViewById(R.id.videoView);
+        videoPath = findViewById(R.id.videoPath);
 
         LoadVid.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +44,12 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
 
             Uri video = data.getData();
+            Log.d("Video File Path: ", String.valueOf(video));
+
             try {
                 videoView.setVideoURI(video);
                 videoView.start();
+                videoPath.setText(String.valueOf(video));
             } catch (Exception e) {
                 e.printStackTrace();
             }
