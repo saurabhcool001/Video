154216f93efaaa61323dcce4af7bf7b6b6bd0bef
saurabhcool001 <parmarsaurabh87@gmail.com>
2019-02-22 13:21:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.videoplayer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button LoadVid;
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoadVid = findViewById(R.id.LoadVid);
+        videoView = findViewById(R.id.videoView);
+
+        LoadVid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            Uri video = data.getData();
+            try {
+                videoView.setVideoURI(video);
+                videoView.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
